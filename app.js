59f68feb09046d734c0af1b62d2ee03850f05e6f
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-09-16 00:16:38 +0530

    view engine setup

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const path = require('path')
+dotenv.config()
 
 // initializing socket.io that runs on the http server so we need http server
 const socketio = require('socket.io')
 const http = require('http')
-const path = require('path')
-dotenv.config()
 
 const app = express()
 
@@ -13,6 +13,11 @@ const app = express()
 const server = http.createServer(app)
 // calling the socketio function
 const io = socketio(server)
+
+
+// view engine setup(ejs)
+app.set("view engine" , "ejs")
+app.set(express.static(path.join(__dirname , "public")))
 
 app.get("/" , (req,res)=>{ 
     // res.render("index")
