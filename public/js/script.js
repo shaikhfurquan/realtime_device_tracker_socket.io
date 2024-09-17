b22025db849f2f0d78e815615b25c7667a954e04
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-09-17 12:29:07 +0530

    map-view/receive location

--- public/js/script.js
+++ public/js/script.js
@@ -16,3 +16,23 @@ if (navigator.geolocation) {
     })
 }
 
+// Initialize a mao centered at coordinates(0,0) with a zoom level using leafLet.
+const map = L.map("map").setView([0,0] , 20)
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
+    attribution:"OpenStreetMap"
+}).addTo(map)
+
+// create an empty object marker
+const markers = {}
+
+// using the receive-location
+socket.on("receive-location" , (data)=>{
+    // update the marker position
+    const {id , latitude, longitude} = data
+    map.setView([latitude , longitude])
+    if(markers[id]){
+        markers[id].setLatLng([latitude , longitude])
+    }else{
+        markers[id] = L.marker([latitude , longitude]).addTo(map)
+    }
+})
